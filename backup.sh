5c96d3268cc49e1d75cc8e2ebbaff8e8bbf64df2
Quentin Wang <wangkx0105@outlook.com>
2018-09-23 00:59:48 +0800

    backup source files

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 rm backup_source_files.tar.gz
-tar zcvf backup_source_files.tar.gz ./source/ ./themes
+tar zcvf backup_source_files.tar.gz ./source/ ./themes ./node_modules
 
 git add .
 git commit -m 'backup source files'
