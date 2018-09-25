f33e2be3af89e158d19fa95ddcc07feff503a583
Quentin Wang <wangkx0105@outlook.com>
2018-09-25 23:49:30 +0800

    backup source files

--- backup.sh
+++ backup.sh
@@ -3,6 +3,6 @@ tar zcvf backup_source_files.tar.gz ./source/ ./themes ./node_modules
 
 git add .
 git commit -m 'backup source files'
-git push origin master:Hexo-Blog
+git push origin master:Hexo-Blog-Backup
 
 echo "backup finished !"
